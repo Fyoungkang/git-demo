42532eff5169850bebb4eb49731472b4b36bd988
Fengyongkang <916479982@qq.com>
2022-08-09 10:56:01 +0800

    github-test1

--- git-test/src/main/java/com/yongkang/gittest/ctrl/Test.java
+++ git-test/src/main/java/com/yongkang/gittest/ctrl/Test.java
@@ -9,6 +9,6 @@ import java.util.Date;
 public class Test {
     @RequestMapping("/test")
     public String getTest(){
-        return "getTest11111" + new Date();
+        return "getTest11111" + new Date() + "15";
     }
 }

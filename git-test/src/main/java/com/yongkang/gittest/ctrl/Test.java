6d781dadc948acdc426181077045093e681a429f
Fengyongkang <916479982@qq.com>
2022-08-09 09:43:10 +0800

    github-test1

--- git-test/src/main/java/com/yongkang/gittest/ctrl/Test.java
+++ git-test/src/main/java/com/yongkang/gittest/ctrl/Test.java
@@ -3,10 +3,12 @@ package com.yongkang.gittest.ctrl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class Test {
     @RequestMapping("/test")
     public String getTest(){
-        return "getTest" + new Date();
+        return "getTest11111" + new Date();
     }
 }
